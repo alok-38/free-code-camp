a0b9731d3e261d84c8460e52ab66e716572de9c0
Alokananda Y <plantsfortrees@gmail.com>
2024-02-08 14:37:42 +0530

    RPG

---
+++ fullstack-development/learn-javascript/rpg-game/script.js
@@ -0,0 +1,16 @@
+// Variables
+const XP = 0;
+const HEALTH = 100;
+const GOLD = 50;
+
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1El = document.getElementById("button1");
+const button2El = document.getElementById("button2");
+const button3El = document.getElementById("button3");
+
+
+
