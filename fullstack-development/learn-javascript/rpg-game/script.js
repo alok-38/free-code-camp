eb10a1ac8e5b28d7313400f55cab7491bff7b4ec
Alokananda Y <plantsfortrees@gmail.com>
2024-02-08 15:01:02 +0530

    RPG

--- fullstack-development/learn-javascript/rpg-game/script.js
+++ fullstack-development/learn-javascript/rpg-game/script.js
@@ -18,6 +18,13 @@ const goldTextEl = document.getElementById("goldText");
 const monsterStatsEl = document.getElementById("monsterStats");
 const monsterNameEl = document.getElementById("monsterName");
 const monsterHealthEl = document.getElementById("monsterHealth");
+// Empty array to store the locations
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"]
+    }
+];
 
 // Event listeners for the buttons
 button1El.onclick = goStore;
@@ -26,7 +33,12 @@ button3El.onclick = fightDragon;
 
 // Functions for visting stores, cave and fight the dragon
 function goStore() {
-
+    button1El.innerText = "Buy 10 health (10 gold)";
+    button2El.innerText = "Buy weapon (30 gold)";
+    button3El.innerText = "Go to town square";
+    button1El.onclick = buyHealth;
+    button2El.onclick = buyWeapon;
+    button3El.onclick = goTown;
 }
 
 function goCave() {
@@ -37,3 +49,20 @@ function fightDragon() {
 
 }
 
+function buyHealth() {
+    
+}
+
+function buyWeapon() {
+    
+}
+
+function goTown() {
+    button1El.innerText = "Go to store";
+    button2El.innerText = "go to cave";
+    button3El.innerText = "Fight dragon";
+    button1El.onclick = buyHealth;
+    button2El.onclick = buyWeapon;
+    button3El.onclick = goTown;
+    text.innerText = "You are in the town square. Do you see the sign \"Store\"?";
+}
