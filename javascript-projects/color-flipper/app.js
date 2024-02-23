3360ff53dbb1e9d374501621d515103f6aac1140
Alokananda Y <plantsfortrees@gmail.com>
2024-02-23 15:20:59 +0530

    flipper

--- javascript-projects/color-flipper/app.js
+++ javascript-projects/color-flipper/app.js
@@ -1,4 +1,19 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
+
 const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const colorSpan = document.querySelector(".color");
+const colorPicker = document.getElementById("color-picker");
+
+btn.addEventListener('click', () => {
+    const color = colorPicker.value;
+    // Invoke a function
+    changeColor(color);
+});
+
+const changeColor = (color) => {
+    document.body.style.backgroundColor = color;
+    colorSpan.textContent = color;
+}
 
