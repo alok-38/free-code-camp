a8136ae8b1d7c0aa565cebf1913c8dd539c5fde9
Alokananda Y <plantsfortrees@gmail.com>
2024-03-14 09:59:16 +0530

    react

---
+++ react-projects/basic-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import YouTube from './Youtube';
+
+function App() {
+  return (
+    <>
+      <h1>Classic react App</h1>
+      <YouTube />
+    </>
+  );
+}
+
+export default App;
---
+++ react-projects/basic-react/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function YouTube() {
+    return (
+        <h1>YouTube react app</h1>
+    )
+}
+
+export default YouTube
