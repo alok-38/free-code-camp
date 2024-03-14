29ff51a6043c732006790880398eec41aa751dc2
Alokananda Y <plantsfortrees@gmail.com>
2024-03-14 10:46:01 +0530

    tailwind

---
+++ react-projects/tailwind-props/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let myObj = {
+    name: 'Alok',
+    age: 39,
+    address: {
+      city: 'Bangalore',
+      state: 'Karnataka',
+      country: 'India'
+    }
+  }
+  let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  return (
+    <>
+      <h1 className='text-5xl bg-green-500 p-3 rounded-md'>Vite with Tailwind</h1>
+      <Card username="alok" myArr={newArr} />
+      <Card username='Json' post='Staff Engg.' />
+    </>
+  )
+}
+
+export default App
