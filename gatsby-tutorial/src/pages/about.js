5e58f0e96223be119e2ca545c280bc2351ca293a
Alokananda Y <plantsfortrees@gmail.com>
2024-03-18 07:40:37 +0530

    links

--- gatsby-tutorial/src/pages/about.js
+++ gatsby-tutorial/src/pages/about.js
@@ -1,15 +1,19 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 // Step 2: Define your component
 const AboutPage = () => {
     return (
         <main>
+            <Link to="/">Home</Link>
             <h1>About Me</h1>
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
         </main>
     )
 }
+
+export const Head = () => <title>About Me</title>
 
 // Step 3: Export your component
 export default AboutPage
