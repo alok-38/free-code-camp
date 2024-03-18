777567bb6c76b1576c5fd5ffb2f00f9ab4bbddfa
Alokananda Y <plantsfortrees@gmail.com>
2024-03-18 07:14:47 +0530

    Gatsby

---
+++ gatsby-tutorial/src/pages/index.js
@@ -0,0 +1,18 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  )
+}
+
+// You will learn about this in the next task
+export const Head = () => <title>Home Page</title>
+
+// Step 3: Export your component
+export default IndexPage
