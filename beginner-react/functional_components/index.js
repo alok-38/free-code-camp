19880a500b11a7edfcfd2849d8ea2310a9d8387d
Alokananda Y <plantsfortrees@gmail.com>
2024-03-26 14:23:25 +0530

    list

--- beginner-react/functional_components/index.js
+++ beginner-react/functional_components/index.js
@@ -1,11 +1,10 @@
-// Define a simple functional component
 function MyInfo() {
   return (
     <div className="h-screen flex flex-col justify-center items-center max-w-7xl">
       <h1 className="text-3xl font-bold underline mb-3">Alokananda Y</h1>
       <p className="leading-loose mb-5">Welcome to my first React app!</p>
       <p className="mb-5">Here are some places I would like to visit:</p>
-      <ol className="list-none leading-10">
+      <ol className="list-none leading-10 pr-52">
         <li>India</li>
         <li>USA</li>
         <li>UK</li>
@@ -14,6 +13,5 @@ function MyInfo() {
   );
 }
 
-// Use createRoot to render the Greeting component to the root element in the HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MyInfo />);
