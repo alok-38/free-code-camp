baaf7b0fdd81533300595087b63277fb26525860
Alokananda Y <plantsfortrees@gmail.com>
2024-03-26 17:31:59 +0530

    tail

---
+++ beginner-react/todo-app/src/components/InputArea.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Circle from "../images/circle.svg";
+// import { list as data } from "./data";
+
+function InputArea({ handleSubmit }) {
+    const [input, setInput] = useState("");
+
+    return (
+        <div
+            id="#input"
+            className="flex w-full h-16 px-6 my-12 text-lg leading-tight text-gray-700 align-middle bg-white rounded shadow appearance-none dark:bg-input-dark focus:outline-none focus:shadow-outline"
+        >
+            <div>
+                <img src={Circle} alt="LogoCentang" className="mt-5 mr-6" />
+            </div>
+
+            <form
+                className="flex-1"
+                onSubmit={(e) => {
+                    {
+                        handleSubmit(e, input);
+                        setInput("");
+                    }
+                }}
+            >
+                <input
+                    className="w-full h-16 border-none input dark:bg-input-dark dark:text-gray-300"
+                    id="username"
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="What to do ?"
+                />
+            </form>
+        </div>
+    );
+}
+
+export default InputArea;
