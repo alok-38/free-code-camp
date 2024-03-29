a8de0d9e1a4c6aab681da9521769bdc323a0fa82
Alokananda Y <plantsfortrees@gmail.com>
2024-03-29 14:16:01 +0530

    react

--- bob-ziroll/fun-facts/src/App.js
+++ bob-ziroll/fun-facts/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Header from './components/Header';
 import MainContent from './components/MainContent';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <MainContent />
     </div>
   );
--- bob-ziroll/fun-facts/src/components/Navbar.jsx
+++ bob-ziroll/fun-facts/src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import logo from '../logo.svg';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div>
+            <div className='navbar__left'>
                 <img src={logo} className="App-logo" alt="logo" />
                 <h3>ReactFacts</h3>
             </div>
-            <div>
+            <div className='navbar__right'>
                 <p>React Course - Project 1</p>
             </div>
         </nav>
