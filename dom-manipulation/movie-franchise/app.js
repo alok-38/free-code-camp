c47b311ad0bfddffc0e063ca94ff2a3fa01389d1
Alokananda Y <plantsfortrees@gmail.com>
2024-04-02 18:47:19 +0530

    dom

---
+++ dom-manipulation/movie-franchise/app.js
@@ -0,0 +1,6 @@
+// Selecting elements
+const mainHeading = document.getElementById('main-heading');
+const listItems = document.querySelectorAll('.list-items');
+
+// Modifying content
+mainHeading.textContent = 'Updated Movie Franchise';
