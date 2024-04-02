85082115ff434a8917781e096a7e9896e3f4260f
Alokananda Y <plantsfortrees@gmail.com>
2024-04-02 19:07:05 +0530

    Updated Movie Franchise

--- dom-manipulation/movie-franchise/app.js
+++ dom-manipulation/movie-franchise/app.js
@@ -2,5 +2,24 @@
 const mainHeading = document.getElementById('main-heading');
 const listItems = document.querySelectorAll('.list-items');
 
-// Modifying content
-mainHeading.textContent = 'Updated Movie Franchise';
+// Adding event listener to each list item
+listItems.forEach(item => {
+    item.addEventListener('click', () => {
+        alert(`You clicked on ${item.textContent}`);
+    });
+});
+
+const addButtonElement = document.getElementById("add-button");
+const ulElement = document.querySelector("ul");
+
+addButtonElement.addEventListener('click', function() {
+    const newMovie = prompt("Enter a new movie franchise:");
+    if (newMovie) {
+        const newListItem = document.createElement("li");
+        newListItem.textContent = newMovie;
+        newListItem.classList.add("list-items");
+        ulElement.appendChild(newListItem);
+        // Update the main heading only if a new movie franchise is added
+        mainHeading.textContent = 'Updated Movie Franchise';
+    }
+});
