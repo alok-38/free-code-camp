43ae2aed9fab46b2facc14b677f2962dbbe54f5c
Alokananda Y <plantsfortrees@gmail.com>
2024-04-04 08:22:03 +0530

    events

--- dom-manipulation/event-listeners/app.js
+++ dom-manipulation/event-listeners/app.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const btn2 = document.querySelector('.btn-2');
     const btn3 = document.querySelector('.btn-3');
     const boxOneElement = document.querySelector('.box-1');
+   // Selecting the box two element
+    const boxTwoElement = document.querySelector('.box-2');
+
+    // Selecting the heading element within box two
+    const headingBox2Element = document.querySelector('.box-2 #heading');
 
     // Event listener for button 1
     btn1.addEventListener('click', () => {
@@ -15,14 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
         boxOneElement.appendChild(newElement);
     });
 
-    // Event listener for button 2
+    // Adding event listener for button 2
     btn2.addEventListener('click', () => {
         mainHeading.textContent = "Button 2 clicked!";
+        boxTwoElement.style.backgroundColor = 'rebeccapurple';
+        headingBox2Element.style.color = '#ffffff';
     });
 
     // Event listener for button 3
     btn3.addEventListener('click', () => {
         mainHeading.textContent = "Button 3 clicked!";
+        boxTwoElement.style.backgroundColor = 'orange';
+        headingBox2Element.style.color = '#4a4848';
     });
 
 });
