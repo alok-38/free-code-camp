d274e82ca783b12169dedb3b273330863ec43df7
Alokananda Y <plantsfortrees@gmail.com>
2024-04-04 15:56:51 +0530

    list

--- dom-manipulation/dom-example/shopping-list/app.js
+++ dom-manipulation/dom-example/shopping-list/app.js
@@ -22,7 +22,45 @@ button.addEventListener('mouseleave', () => {
     main.style.backgroundColor = '';
     headingElement.style.color = 'aliceblue';
     labelElement.style.color = 'aliceblue';
-    inputElement.style.backgroundColor = '#f6c0b6';
     button.style.backgroundColor = 'aliceblue';
     button.style.color = 'tomato';
 });
+
+// Unordered list to hold the list 
+const unorderedList = document.createElement('ul');
+// Append the unordered list as a child of the main element
+main.appendChild(unorderedList);
+
+const respondWhenClicked = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    //  storing the current value of the input element in a variable
+    const newItem = inputElement.value;
+    // Set input element value to empty string
+    inputElement.value = '';
+    const listElement = document.createElement('li');
+    const spanElement = document.createElement('span');
+    const newButton = document.createElement('button');
+    // Append the span and button elements as children of the list item
+    listElement.appendChild(spanElement);
+    listElement.appendChild(newButton);
+    // Set the text content of the span to the input element value
+    spanElement.textContent = newItem; // Changed inputElement.value to newItem
+
+    // Set the text content of the button to 'Delete'
+    newButton.textContent = 'Delete';
+    // Set the class
+    newButton.classList = 'delete-btn';
+
+    // Append the list item as a child of the unordered list
+    unorderedList.appendChild(listElement);
+
+    // Attach an event handler to the delete button
+    newButton.addEventListener('click', () => {
+        // Remove the list item when the delete button is clicked
+        listElement.remove();
+        // Use the focus() method to focus the input element
+        inputElement.focus();
+    });
+}
+
+button.addEventListener('click', respondWhenClicked);
