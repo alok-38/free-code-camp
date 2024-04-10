c8aa40b9a78913491e693b54d7b04fad90eddd03
Alokananda Y <plantsfortrees@gmail.com>
2024-04-10 19:28:47 +0530

    router

--- dave-gray/nextjs-course/chapter-01/src/app/about/page.tsx
+++ dave-gray/nextjs-course/chapter-01/src/app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-      <h1>About</h1>
+      <h1 className="p-20">About</h1>
       <Link className="text-purple-500" href="/">Back Home</Link>
     </>
   );
