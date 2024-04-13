de7413363dc7c863a8fb5b4e8fe9ae69d947760b
Alokananda Y <plantsfortrees@gmail.com>
2024-04-13 16:46:50 +0530

    none

--- beginner-react/hell-world/app.js
+++
@@ -1,8 +0,0 @@
-const app = document.getElementById("app");
-
-const h1Element = document.createElement('h1');
-h1Element.textContent = 'Hellish World';
-app.appendChild(h1Element);
-
-
-const root = ReactDOM.createRoot(app);
--- react-projects/basic-react/src/Youtube.jsx
+++
@@ -1,7 +0,0 @@
-function YouTube() {
-    return (
-        <h1>YouTube react app</h1>
-    )
-}
-
-export default YouTube
