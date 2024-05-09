9fcd39f8bf678554bc32b6455ca7d490af6d4701
Alokananda Y <plantsfortrees@gmail.com>
2024-05-09 08:02:16 +0530

    refactored

--- dave-gray/react-tutorial/groceries-list/src/components/Content.jsx
+++ dave-gray/react-tutorial/groceries-list/src/components/Content.jsx
@@ -19,14 +19,27 @@ export default function Content() {
       item: "Item 3",
     },
   ]);
+
+  const handleCheckboxChange = (itemId) => {
+    const updatedItems = items.map((item) =>
+      item.id === itemId ? { ...item, checked: !item.checked } : item
+    );
+    setItems(updatedItems);
+  };
+
   return (
     <main>
       <ul>
         {items.map((item) => (
-          <li className="flex items-center justify-between" key={item.id}>
-            <input className="mr-5" type="checkbox" name="" id="" />
+          <li className="flex items-center justify-between text-slate-600" key={item.id}>
+            <input
+              className="mr-5 w-6 h-6 cursor-pointer transform hover:scale-110 transition duration-300"
+              type="checkbox"
+              checked={item.checked}
+              onChange={() => handleCheckboxChange(item.id)}
+            />
             <label htmlFor="">{item.item}</label>
-            <FaTrashAlt className="ml-10 cursor-pointer text-red-500 transition duration-300 transform hover:scale-110" />
+            <FaTrashAlt className="ml-10 cursor-pointer text-red-500 transition duration-300 transform hover:scale-110 w-6 h-6" />
           </li>
         ))}
       </ul>
