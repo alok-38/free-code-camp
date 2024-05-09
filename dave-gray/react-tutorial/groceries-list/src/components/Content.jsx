501ba8862e822a67399d07ce42a261b6199b02bf
Alokananda Y <plantsfortrees@gmail.com>
2024-05-09 07:15:20 +0530

    map

--- dave-gray/react-tutorial/groceries-list/src/components/Content.jsx
+++ dave-gray/react-tutorial/groceries-list/src/components/Content.jsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 export default function Content() {
-  const [currentName, setCurrentName] = useState("John");
-  const names = [
-    "John",
-    "Emma",
-    "Michael",
-    "Sophia",
-    "William",
-    "Olivia",
-    "James",
-    "Ava",
-    "Alexander",
-    "Mia",
-  ]; // Array of names
-
-  const generateRandomName = () => {
-    const randomIndex = Math.floor(Math.random() * names.length);
-    const randomName = names[randomIndex];
-    setCurrentName(randomName);
-  };
-
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pund bag of Cocoa covered Almond mix",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
   return (
-    <div>
-      <h1>Current Name: {currentName}</h1>
-      <button onClick={generateRandomName}>Generate Random Name</button>
-    </div>
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <input className="mr-5" type="checkbox" name="" id="" />
+            <label htmlFor="">{item.item}</label>
+            <button className="pl-5">Delete</button>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
