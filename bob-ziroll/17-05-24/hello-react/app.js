21314f56700b99c68da0f535a3fa8caeb37dbcd0
Alokananda Y <plantsfortrees@gmail.com>
2024-05-17 07:03:07 +0530

    Hello from react

--- bob-ziroll/17-05-24/hello-react/app.js
+++ bob-ziroll/17-05-24/hello-react/app.js
@@ -1,7 +1,27 @@
 const app = document.getElementById("app");
 
 const Hello = () => {
-  return <h1>Hello from React!</h1>;
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <FavoriteThings />
+    </div>
+  );
+};
+
+const FavoriteThings = () => {
+  return (
+    <div>
+		<h2>Things I love to do...</h2>
+      <ul>
+        <li>Drink Coffee</li>
+        <li>Occasionally read books</li>
+        <li>Code often</li>
+        <li>Listen to music</li>
+        <li>Workout regularly</li>
+      </ul>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(app);
