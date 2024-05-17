025d9495426407749736e6682a29d4727686070c
Alokananda Y <plantsfortrees@gmail.com>
2024-05-17 11:04:55 +0530

    static site

--- bob-ziroll/17-05-24/static-site/src/components/Main.jsx
+++ bob-ziroll/17-05-24/static-site/src/components/Main.jsx
@@ -1,22 +1,8 @@
-import Navbar from "./Navbar";
-import { FaHeart } from "react-icons/fa";
-
+import Header from "./Header";
 export default function Main() {
   return (
-    <div>
-      <Navbar />
-      <div className="m-10 text-slate-700">
-        <h1 className="mb-5 text-5xl">My awesome website in React</h1>
-        <h3 className="mb-2 text-3xl">
-          Reasons why I <FaHeart className="inline text-red-500" /> React
-        </h3>
-        <ul className="leading-10 pl-5">
-          <li>It&apos;s composable</li>
-          <li>It&apos;s declarative</li>
-          <li>It&apos;s a hireable skill</li>
-          <li>It&apos;s maintained by skilled people</li>
-        </ul>
-      </div>
-    </div>
+    <main className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+      <Header />
+    </main>
   );
 }
