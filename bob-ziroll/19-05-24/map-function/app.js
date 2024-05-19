f0ae37a472f494c79a00322bc587b9450a5831f4
Alokananda Y <plantsfortrees@gmail.com>
2024-05-19 08:20:40 +0530

    SquaredList

--- bob-ziroll/19-05-24/map-function/app.js
+++ bob-ziroll/19-05-24/map-function/app.js
@@ -1,7 +1,16 @@
 const app = document.getElementById("app");
 const nums = [1, 2, 3, 4, 5];
-const squared = nums.map(() => {
+const squared = nums.map((num) => num * num);
 
-})
+function SquaredList({ numbers }) {
+  return (
+    <ul>
+      {numbers.map((number, index) => (
+        <li key={index}>{number}</li>
+      ))}
+    </ul>
+  );
+}
+
 const root = ReactDOM.createRoot(app);
-root.render(<squared />);
+root.render(<SquaredList numbers={squared} />);
