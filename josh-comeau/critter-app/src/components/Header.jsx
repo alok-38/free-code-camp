3acaf77e386d1cddb1e9a85159488912695c1d1a
Alokananda Y <plantsfortrees@gmail.com>
2024-06-11 14:54:22 +0530

    Nothing

---
+++ josh-comeau/critter-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  const message = {
+    author: {
+      avatarSrc: "https://sandpack-bundler.vercel.app/img/avatars/009.png",
+      avatarDescription: "Cartoon bear",
+      name: "Ben Thorn",
+      handle: "benjaminthorn",
+    },
+  };
+  return (
+	<header>
+
+	</header>
+  )
+}
