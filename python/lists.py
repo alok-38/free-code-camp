adbeb9021ed90b065332d49c767303f42c94b84d
ThePrimeagen <the.primeagen@aol.com>
2026-07-13 12:06:16 +0530

    flask blog

--- python/lists.py
+++ python/lists.py
@@ -4,3 +4,23 @@ print(numbers)
 even_numbers = [6, 8, 10]
 numbers.append(even_numbers)
 print(numbers)
+numbers.insert(2, 2.5)
+print(numbers)
+numbers = [10, 20, 30, 40, 50, 50]
+numbers.remove(50)
+print(numbers)
+numbers = [1, 2, 3, 4, 5]
+numbers.pop(1)
+numbers.clear()
+print(numbers)
+numbers = [19, 2, 35, 1, 67, 41]
+numbers.sort()
+print(numbers)
+numbers = [19, 2, 35, 1, 67, 41]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+programming_languages = ['Rust', 'Java', 'Python', 'C++']
+print(programming_languages.index("Java"))
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(2, 2.5)
+print(numbers)
